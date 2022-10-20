7fc5b6f3e9cea50858e95d035787e4597c4a47f9
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-10-20 14:14:24 -0700

    Create PersonalInfo.java

---
+++ PersonalInfo.java
@@ -0,0 +1,11 @@
+// Display all Personal Info on separate lines with a single println
+
+public class PersonalInfo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("David Wylie\n123 Fake Street\n" +
+		                   "Springfield, OH 45501\n(555) 555-1234" +
+		                   "\nMajor: CIS");
+	}
+}
