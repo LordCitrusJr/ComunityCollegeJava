04a0358be344f15a4c4977f97b92082bbf0be694
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-10-20 14:19:24 -0700

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,10 @@
+// Make a Star Pattern
+
+public class StarPattern
+{
+	public static void main(String[] args)
+	{
+		System.out.print(" *\n ***\n *****\n*******\n" +
+		                 " *****\n ***\n *");
+	}
+}
